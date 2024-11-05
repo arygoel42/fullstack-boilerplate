3b65ae6f38aad60eb0a8c93e98397727f51091cf
aryangoel <aryangoel574@gmail.com>
2024-11-05 12:11:32 -0500

    example

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("hello world");
+router.post("/", (req, res) => {
+  const resource = req.body.resource;
+
+  if (resource) {
+    res.status(200).send(resource);
+  } else if (!resource) {
+    res.status(400).send("bad request");
+  }
 });
 
 module.exports = router;
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { useState } from "react";
+import axios from "axios";
+const App = () => {
+  let resource = ["This", "is", "sent", "to", "the", "backend"];
+  const [recieved, setRecieved] = useState<string>("");
 
-function App() {
-  const [count, setCount] = useState(0)
+  const submitResources = async (resource: string[]) => {
+    try {
+      let response = await axios.post("http://localhost:3011/api/users/", {
+        resource: resource,
+      });
+
+      if (response.status === 200) {
+        setRecieved(response.data);
+      } else if (response.status === 400 || response.status === 500) {
+        setRecieved("error in returning data ");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <button
+        onClick={() => {
+          submitResources(resource);
+        }}
+      ></button>
 
-export default App
+      {recieved != "" ? <div>{recieved}</div> : ""}
+    </div>
+  );
+};
+
+export default App;
